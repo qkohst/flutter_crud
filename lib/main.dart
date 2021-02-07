4b11fb7781129c467edb69ef139a10c0b1464ca0
qkohst <7311626-qkohst@users.noreply.gitlab.com>
2021-02-07 21:11:01 +0700

    add fungsi login print response

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MaterialApp(
@@ -13,26 +16,56 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   String username, password;
+  final _key = new GlobalKey<FormState>();
+
+  check() {
+    final form = _key.currentState;
+    if (form.validate()) {
+      form.save();
+      login();
+    }
+  }
+
+  login() async {
+    final response = await http.post(
+        "http://192.168.0.102/flutter_crud/api/login.php",
+        body: {"username": username, "password": password});
+    final data = jsonDecode(response.body);
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(
-        children: <Widget>[
-          TextFormField(
-            onSaved: (e) => username = e,
-            decoration: InputDecoration(labelText: "Username"),
-          ),
-          TextFormField(
-            obscureText: true,
-            onSaved: (e) => password = e,
-            decoration: InputDecoration(labelText: "Password"),
-          ),
-          MaterialButton(
-            onPressed: null,
-            child: Text("Login"),
-          )
-        ],
+      body: Form(
+        key: _key,
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            TextFormField(
+              // Untuk Validasi Jika Form Kosong
+              validator: (e) {
+                if (e.isEmpty) {
+                  return "Please insert username";
+                }
+              },
+              onSaved: (e) => username = e,
+              decoration: InputDecoration(labelText: "Username"),
+            ),
+            TextFormField(
+              obscureText: true,
+              onSaved: (e) => password = e,
+              decoration: InputDecoration(labelText: "Password"),
+            ),
+            MaterialButton(
+              onPressed: () {
+                check();
+              },
+              child: Text("Login"),
+            )
+          ],
+        ),
       ),
     );
   }
