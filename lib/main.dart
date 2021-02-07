0a0fe10c35091cdb8668916983adfde266777920
qkohst <7311626-qkohst@users.noreply.gitlab.com>
2021-02-07 22:58:04 +0700

    Add Home Menu & Shared Preferences

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,7 +15,13 @@ class Login extends StatefulWidget {
   _LoginState createState() => _LoginState();
 }
 
+enum LoginStatus {
+  notSignIn,
+  signIn,
+}
+
 class _LoginState extends State<Login> {
+  LoginStatus _loginStatus = LoginStatus.notSignIn;
   String username, password;
   final _key = new GlobalKey<FormState>();
 
@@ -39,50 +46,111 @@ class _LoginState extends State<Login> {
         "http://192.168.0.102/flutter_crud/api/login.php",
         body: {"username": username, "password": password});
     final data = jsonDecode(response.body);
-    print(data);
+    int status = data["status"];
+    String message = data["message"];
+    if (status == 200) {
+      setState(() {
+        _loginStatus = LoginStatus.signIn;
+        savePref(status);
+      });
+      print(message);
+    } else {
+      print(message);
+    }
   }
 
+  savePref(int status) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    setState(() {
+      preferences.setInt("status", status);
+      preferences.commit();
+    });
+  }
+
+  var status;
+  getPref()async{
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    setState(() {
+      status = preferences.getInt("status");
+
+      _loginStatus = status == 200 ? LoginStatus.signIn : LoginStatus.notSignIn;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPref();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    switch (_loginStatus) {
+      case LoginStatus.notSignIn:
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Flutter CRUD"),
+          ),
+          body: Form(
+            key: _key,
+            child: ListView(
+              padding: EdgeInsets.all(16.0),
+              children: <Widget>[
+                TextFormField(
+                  // Untuk Validasi Jika Form Kosong
+                  validator: (e) {
+                    if (e.isEmpty) {
+                      return "Please insert username";
+                    }
+                  },
+                  onSaved: (e) => username = e,
+                  decoration: InputDecoration(labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: _secureText,
+                  onSaved: (e) => password = e,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    suffixIcon: IconButton(
+                      onPressed: showHide,
+                      icon: Icon(_secureText
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                    ),
+                  ),
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    check();
+                  },
+                  child: Text("Login"),
+                )
+              ],
+            ),
+          ),
+        );
+        break;
+      case LoginStatus.signIn:
+        return MainMenu();
+        break;
+    }
+  }
+}
+
+class MainMenu extends StatefulWidget {
+  @override
+  _MainMenuState createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter CRUD"),
       ),
-      body: Form(
-        key: _key,
-        child: ListView(
-          padding: EdgeInsets.all(16.0),
-          children: <Widget>[
-            TextFormField(
-              // Untuk Validasi Jika Form Kosong
-              validator: (e) {
-                if (e.isEmpty) {
-                  return "Please insert username";
-                }
-              },
-              onSaved: (e) => username = e,
-              decoration: InputDecoration(labelText: "Username"),
-            ),
-            TextFormField(
-              obscureText: _secureText,
-              onSaved: (e) => password = e,
-              decoration: InputDecoration(
-                labelText: "Password",
-                suffixIcon: IconButton(
-                  onPressed: showHide,
-                  icon: Icon(
-                      _secureText ? Icons.visibility_off : Icons.visibility),
-                ),
-              ),
-            ),
-            MaterialButton(
-              onPressed: () {
-                check();
-              },
-              child: Text("Login"),
-            )
-          ],
-        ),
+      body: Center(
+        child: Text("Menu Utama"),
       ),
     );
   }
